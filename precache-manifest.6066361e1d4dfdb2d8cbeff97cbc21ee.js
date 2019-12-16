002f6d27dbb36a4fce5a933de77932f46d64163e
okkisatriar <o.satriarahmat@gmail.com>
2019-12-16 12:07:49 +0700

    Menyalin isi folder build

---
+++ precache-manifest.6066361e1d4dfdb2d8cbeff97cbc21ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d18c710dde8d027ef3ae",
+    "url": "/static/css/main.780f9a8f.chunk.css"
+  },
+  {
+    "revision": "d18c710dde8d027ef3ae",
+    "url": "/static/js/main.d18c710d.chunk.js"
+  },
+  {
+    "revision": "eb8680aee82c29f2c615",
+    "url": "/static/js/1.eb8680ae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0eac8c9e8c035ad04e8b7213c7d2d801",
+    "url": "/index.html"
+  }
+];
